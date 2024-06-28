a13fe4489376535c4753bb67393ac9310af6c26b
Deepu <Deepu@DESKTOP-HMNEQGA>
2024-06-28 12:13:19 +0530

    Assignment 3

---
+++ corejava/src/inheritance/Country.java
@@ -0,0 +1,30 @@
+package inheritance;
+
+public class Country extends Contient
+{
+		 String countryName;
+		 int noofstates;
+		 String climate;
+		 
+		 public Country() {
+			 super();
+		 }
+
+		public Country(String continentName, double size,int noofcountries, String countryName,int noofstates,String climate) {
+			super(continentName, size, noofcountries);
+			this.countryName = countryName;
+			this.noofstates = noofstates;
+			this.climate = climate;
+		}
+
+		@Override
+		public String toString() {
+			return "Country [countryName=" + countryName + ", noofstates=" + noofstates + ", climate=" + climate
+					+ ", continentName=" + continentName + ", size=" + size + ", noofcountries=" + noofcountries
+					+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+					+ "]";
+		}
+		
+		
+	}
+
